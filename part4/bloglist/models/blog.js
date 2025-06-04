f1c082c9e5dc16914dcc2bc999fb270095271c1e
Samiran <samiran.krishnatreya@tuni.fi>
2025-06-04 23:01:22 +0300

    Fix schema naming inconsistency and correct module exports in blog model and middleware

--- part4/bloglist/models/blog.js
+++ part4/bloglist/models/blog.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose')
 
-const bloglistSchema = new mongoose.Schema({
+const blogSchema = new mongoose.Schema({
   title: String,
   author: String,
   url: String,
   likes: Number,
 })
 
-bloglistSchema.set('toJSON', {
+blogSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
     delete returnedObject._id
@@ -15,4 +15,4 @@ bloglistSchema.set('toJSON', {
   }
 })
 
-module.exports = mongoose.model('Blog', bloglistSchema)
+module.exports = mongoose.model('Blog', blogSchema)
